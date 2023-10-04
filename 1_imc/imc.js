9b9ab43b088db514ccae815ad2b0837a63ad1ed5
Gabriela Sena <gabriela.sena07@gmail.com>
2023-10-04 19:58:25 -0300

    input do usuário para cálculo do imc

--- 1_imc/imc.js
+++ 1_imc/imc.js
@@ -1,18 +1,35 @@
-let peso = 62;
-let altura = 1.69;
+import { createInterface } from 'readline';
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function readLineAsync(msg) {
+    return new Promise(resolve => {
+        readline.question(msg, userRes => {
+            resolve(userRes);
+        });
+    });
+}
+
+let peso = await readLineAsync('Digite o seu peso em kg: ');
+let altura = await readLineAsync('Digite a sua altura em m: ');
+
+readline.close();
 
 let imc = peso / (altura * altura);
 
-console.log("seu imc é" + imc);
-
 if (imc < 18.5) {
-    console.log(" e você está abaixo do peso");
+    console.log("seu imc é " + imc.toFixed(2) + "e você está abaixo do peso");
 } else if (imc < 25) {
-    console.log(" e você está com o peso normal");
+    console.log("seu imc é " + imc.toFixed(2) + " e você está com o peso normal");
 } else if (imc >= 25 && imc < 30) {
-    console.log(" e você está com o sobrepeso");
+    console.log("seu imc é " + imc.toFixed(2) + " e você está com o sobrepeso");
 } else if (imc >= 30 && imc < 35) {
-    console.log(" e você está com obesidade grau 1");
+    console.log("seu imc é " + imc.toFixed(2) + " e você está com obesidade grau 1");
 } else if (imc >= 35 && imc < 40) {
-    console.log(" e você está com obesidade grau 2");
-} else console.log(" e você está com obesidade grau 3");
+    console.log("seu imc é " + imc.toFixed(2) + " e você está com obesidade grau 2");
+} else {
+    console.log("seu imc é " + imc.toFixed(2) + " e você está com obesidade grau 3");
+}
