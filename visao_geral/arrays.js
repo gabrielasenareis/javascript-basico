8cfd0d6a857313ae1d89be80df0caba84c1fe67b
Gabriela Sena <gabriela.sena07@gmail.com>
2023-10-08 18:46:04 -0300

    Estudo sobre array

---
+++ visao_geral/arrays.js
@@ -0,0 +1,14 @@
+//               0         1         2
+let frutas = ['banana', 'melao', 'mamao']
+
+// Acessar alguma posição/indice especifica
+// console.log(frutas[2])
+
+
+// Tamanho do array (Quantos itens tem nele)
+// console.log(frutas.length)
+
+// Percorrer um array
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
